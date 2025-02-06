266c8020538b2b915e17086d166d8e00e6c087f9
Omid <omidjalalit@gmail.com>
2025-02-06 17:21:46 +0330

    8 commit

--- src/js/user.js
+++ src/js/user.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, {});
-});
+        var instances = M.Collapsible.init(elems, {});
+    
+        const addUserButton = document.getElementById('btnn')
+        addUserButton.addEventListener('click', ()=>{
+            document.querySelector('.user-modal').classList.add('show')
+            document.querySelector('.modal-back').classList.remove('dis-none')
+        })
+        document.querySelector('.modal-back').addEventListener('click', (e)=>{
+            document.querySelector('.user-modal').classList.remove('show')
+            e.target.classList.add('dis-none')
+        })
+    
+    });
