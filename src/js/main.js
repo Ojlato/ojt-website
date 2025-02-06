e43bad4975932aa74fe3654de54f5278fb47904d
OmidJalaliT <omidjalalit@gmail.com>
2025-02-06 18:49:02 +0330

    10 commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {});
+    var instances = M.Sidenav.init(elems, {
+        edge: "right"
+    });
+
+    console.log(1);
+    
+    new Promise(function(resolve,reject){
+        setTimeout(function(){
+            console.log(2);
+            resolve("ok")
+            reject("Eror 404")
+        },1000)
+
+    }).then(
+        function(response){
+            console.log(3);
+        }).catch(
+            function(error){
+                console.log(error);
+            }
+        )
+
 });
